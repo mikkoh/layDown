9784cf0f218b657504150b003238bcd141f326be
Mikko Haapoja <me@mikkoh.com>
2013-10-28 13:32:21 -0400

    Added buildExample to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,4 +34,5 @@ module.exports = function( grunt ) {
 
 	grunt.registerTask( 'test', [ 'nodeunit' ] );
 	grunt.registerTask( 'default', [ 'watch:example' ] );
+	grunt.registerTask( 'buildExample', [ 'browserify:example' ] );
 };
